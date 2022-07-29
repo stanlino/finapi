85b13e08d9bbb362eb17398b5922e60a589bf867
Stanley Filipe <stanley92filipe@gmail.com>
2022-07-29 17:50:14 -0300

    feat: create account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * CPF string
+ * name string
+ * id uuid
+ * statement []
+ */
+
+app.post("/account", (request, response) => {
+  const {
+    body: { cpf, name },
+  } = request;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({
+      error: "Customer already exists!",
+    });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen("3333", console.log("http://localhost:3333"));
