77953eedc954aabb796772ddca4fbc15bb096f1c
Stanley Filipe <stanley92filipe@gmail.com>
2022-07-29 18:12:20 -0300

    feat: get statement

--- src/index.js
+++ src/index.js
@@ -39,4 +39,18 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({
+      error: "Customer not found",
+    });
+  }
+
+  return response.json(customer.statement);
+});
+
 app.listen("3333", console.log("http://localhost:3333"));
