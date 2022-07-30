7c831db00ed5cf01ff0259437e286530339f2704
Stanley Filipe <stanley92filipe@gmail.com>
2022-07-30 00:25:20 -0300

    feat: create withdraw post method

--- src/index.js
+++ src/index.js
@@ -25,6 +25,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((total, operation) => {
+    if (operation.type === "credit") {
+      return total + operation.amount;
+    }
+
+    return total - operation.amount;
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const {
     body: { cpf, name },
@@ -68,6 +80,29 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
     amount,
     created_at: new Date(),
     type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({
+      error: "Insufficient funds!",
+    });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
   };
 
   customer.statement.push(statementOperation);
